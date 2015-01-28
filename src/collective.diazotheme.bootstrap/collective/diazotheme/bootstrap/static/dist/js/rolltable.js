f68fa5d65d5065d5c1f25e98503c1bcbfd7458a0
adam139 <yuejun.tang@gmail.com>
2015-01-28 20:37:59 +0800

    add mobile theme support for xtshzz

---
+++ src/collective.diazotheme.bootstrap/collective/diazotheme/bootstrap/static/dist/js/rolltable.js
@@ -0,0 +1,62 @@
+var rollspeed = 50;
+var rollspeeds = 50;
+var myInter;
+var myInters;
+function MarqueeV() {
+  var e = document.getElementById("oRollV");
+  var t = document.getElementById("oRollV1");
+  var n = document.getElementById("oRollV2");
+  if(n.offsetTop - e.scrollTop <= 0) {
+    e.scrollTop -= t.offsetHeight
+  } else {
+    e.scrollTop++
+  }
+}
+function MarqueeVs() {
+	  var e = document.getElementById("sRollV");
+	  var t = document.getElementById("sRollV1");
+	  var n = document.getElementById("sRollV2");
+	  if(n.offsetTop - e.scrollTop <= 0) {
+	    e.scrollTop -= t.offsetHeight
+	  } else {
+	    e.scrollTop++
+	  }
+	}
+function StartRollV() {
+  var e = document.getElementById("oRollV");
+  var t = document.getElementById("oRollV1");
+  var n = document.getElementById("oRollV2");
+  if(e) {
+    if(parseInt(e.style.height, 10) >= n.offsetTop) {
+      e.style.height = n.offsetTop;
+      return
+    }
+    n.innerHTML = t.innerHTML;
+    myInter = setInterval(MarqueeV, rollspeed);
+    e.onmouseover = function() {
+      clearInterval(myInter)
+    };
+    e.onmouseout = function() {
+      myInter = setInterval(MarqueeV, rollspeed)
+    }
+  }
+}
+function StartRollVs() {
+	  var e = document.getElementById("sRollV");
+	  var t = document.getElementById("sRollV1");
+	  var n = document.getElementById("sRollV2");
+	  if(e) {
+	    if(parseInt(e.style.height, 10) >= n.offsetTop) {
+	      e.style.height = n.offsetTop;
+	      return
+	    }
+	    n.innerHTML = t.innerHTML;
+	    myInters = setInterval(MarqueeVs, rollspeeds);
+	    e.onmouseover = function() {
+	      clearInterval(myInters)
+	    };
+	    e.onmouseout = function() {
+	      myInters = setInterval(MarqueeVs, rollspeeds)
+	    }
+	  }
+	}
